4893ad7afab96cb2b085a8873cdd9a9b9de36f33
codeengineer2 <maximilian.zangerl@student.htl-rankweil.at>
2025-05-08 14:20:19 +0200

    Solution erstellt

---
+++ src/frontend/Structurio/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Structurio
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
