a5231eb2af12b59c590ac2c357c6718f48a03c57
codeengineer2 <maximilian.zangerl@student.htl-rankweil.at>
2025-05-09 12:19:40 +0200

    Page Entwurf erstellt! InitializeComponent Error

--- src/frontend/Structurio/MainWindow.xaml.cs
+++ src/frontend/Structurio/MainWindow.xaml.cs
@@ -20,5 +20,7 @@ namespace Structurio
         {
             InitializeComponent();
         }
+
+        
     }
 }
---
+++ src/frontend/Structurio/TimeStamp.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Structurio
+{
+    /// <summary>
+    /// Interaction logic for TimeStamp.xaml
+    /// </summary>
+    public partial class TimeStamp : Page
+    {
+        public TimeStamp()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the current date and time
+            DateTime now = DateTime.Now;
+
+            // Format the date and time as a string
+            string formattedDate = now.ToString("yyyy-MM-dd HH:mm:ss");
+
+            
+        }
+    }
+}
