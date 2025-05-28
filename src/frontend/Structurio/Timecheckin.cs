dcf02dbdd09d1338a268cc2e8843a0143173dd30
codeengineer2 <maximilian.zangerl@student.htl-rankweil.at>
2025-05-28 21:16:10 +0200

    version conflict lösen time

---
+++ src/frontend/Structurio/Timecheckin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structurio
+{
+    public class Timecheckin
+    {
+        public DateTime CheckIN { get; set; }
+        public DateTime CheckOUT { get; set; }
+        public string Duration { get; set; }
+    }
+}
