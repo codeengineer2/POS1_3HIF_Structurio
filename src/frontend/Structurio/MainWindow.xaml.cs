acc91a15f8cbfebf5ba9f9ad2b223ee331a62bf3
FeldherrWeiprecht <drag.moser@outlook.com>
2025-06-06 12:33:03 +0200

    feat: made backend logic for PasswordResetPage and started with backend logic for MainWindow

--- src/frontend/Structurio/MainWindow.xaml.cs
+++ src/frontend/Structurio/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -8,6 +10,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using Structurio.Pages;
+using Structurio.Windows;
 
 namespace Structurio
 {
@@ -16,19 +21,50 @@ namespace Structurio
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DateTime starTime;
+        private DispatcherTimer timer;
+        public Frame MainFramePublic;
+
         public MainWindow()
         {
             InitializeComponent();
+            StartTimer();
+            this.MainFramePublic = this.mainFrame;
+            this.projectsButton.IsChecked = true;
         }
 
-        private void logout_Click(object sender, RoutedEventArgs e)
+        private void StartTimer()
         {
+            // startTime = DateTime.Now;
+            // startet Timer
+        }
 
+        private void UncheckAllMenuItems(object sender)
+        {
+            foreach (var child in LogicalTreeHelper.GetChildren(SidebarMenuPanel))
+            {
+                if (child is ToggleButton btn && !ReferenceEquals(btn, sender))
+                {
+                    btn.IsChecked = false;
+                }
+            }
+        }
+
+        private void projects_Click(object sender, RoutedEventArgs e)
+        {
+            UncheckAllMenuItems(sender);
         }
 
         private void settings_Click(object sender, RoutedEventArgs e)
         {
+            UncheckAllMenuItems(sender);
+        }
 
+        private void logout_Click(object sender, RoutedEventArgs e)
+        {
+            var loginWindow = new LoginWindow();
+            loginWindow.Show();
+            this.Close();
         }
     }
 }
