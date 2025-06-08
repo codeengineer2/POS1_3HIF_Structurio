50e8e00fe4c52a95ae581ce1a5b5a8c5fb86a010
FeldherrWeiprecht <drag.moser@outlook.com>
2025-06-08 15:06:40 +0200

    feat: added request and response classes for login-part communication

---
+++ src/frontend/Structurio/Classes/LoginResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structurio.Classes
+{
+    public class LoginResponse
+    {
+        public bool Success { get; set; }
+        public User User { get; set; }
+        public List<Project> Projects { get; set; }
+    }
+}
