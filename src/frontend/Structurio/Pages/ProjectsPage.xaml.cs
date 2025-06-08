920b2c04a2d77073fa836b3ca627542b940219c9
FeldherrWeiprecht <drag.moser@outlook.com>
2025-06-08 18:40:10 +0200

    feat: created ProjectsPage structure

---
+++ src/frontend/Structurio/Pages/ProjectsPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Structurio.Controls;
+
+namespace Structurio.Pages
+{
+    /// <summary>
+    /// Interaktionslogik für ProjectsPage.xaml
+    /// </summary>
+    public partial class ProjectsPage : Page
+    {
+        public ProjectsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void searchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
