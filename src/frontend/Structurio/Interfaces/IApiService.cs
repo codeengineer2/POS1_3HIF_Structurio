7ab21fa68a263ccd9a7edc52a40a53e0d1cacf43
FeldherrWeiprecht <drag.moser@outlook.com>
2025-06-08 14:51:38 +0200

    feat: created interfaces that are important to setup the swagger connection

---
+++ src/frontend/Structurio/Interfaces/IApiService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Structurio.Classes;
+
+namespace Structurio.Interfaces
+{
+    public interface IApiService
+    {
+        Task<LoginResponse?> LoginAsync(string email, string password);
+        Task<bool> RegisterAsync(RegisterRequest request);
+    }
+}
---
+++ src/frontend/Structurio/Interfaces/IAuthService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Structurio.Classes;
+
+namespace Structurio.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<bool> TryLogin(string email, string password);
+        Task<bool> Register(RegisterRequest request);
+        User? CurrentUser { get; }
+        void Logout();
+    }
+}
