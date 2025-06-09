4314be52e1acbbf80d7c31f6601cc979ee75e3df
FeldherrWeiprecht <drag.moser@outlook.com>
2025-06-09 16:01:58 +0200

    style: made structure and ugly design for AddIssueWindow

---
+++ src/frontend/Structurio/Windows/AddIssueWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Structurio.Windows
+{
+    /// <summary>
+    /// Interaction logic for AddIssueWindow.xaml
+    /// </summary>
+    public partial class AddIssueWindow : Window
+    {
+        public AddIssueWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void descriptionBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
