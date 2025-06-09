123c71102364eb7501c0caca1bfd166b4b73c3af
FeldherrWeiprecht <drag.moser@outlook.com>
2025-06-09 11:12:10 +0200

    style: made structure and ugly design for KanbanPage and created new UserControl -> ProjectIssueControl

---
+++ src/frontend/Structurio/Pages/KanbanPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Structurio.Controls;
+
+namespace Structurio.Pages
+{
+    /// <summary>
+    /// Interaction logic for KanbanPage.xaml
+    /// </summary>
+    public partial class KanbanPage : Page
+    {
+        public KanbanPage()
+        {
+            InitializeComponent();
+        }
+
+        private void addColumn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void titleBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
