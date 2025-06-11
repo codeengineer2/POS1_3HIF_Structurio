d48846df75578cdcd8ee4a869c9ff3e33accee6b
FeldherrWeiprecht <drag.moser@outlook.com>
2025-06-11 16:28:24 +0200

    style: made structure and ugly design for CreateProjectWindow

---
+++ src/frontend/Structurio/Windows/CreateProjectWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Structurio.Windows
+{
+    /// <summary>
+    /// Interaktionslogik für CreateProjectWindow.xaml
+    /// </summary>
+    public partial class CreateProjectWindow : Window
+    {
+        public string ProjectName { get; private set; }
+        public string ProjectDescription { get; private set; }
+        public string ProjectColor { get; private set; }
+
+        public CreateProjectWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(nameBox.Text))
+            {
+                MessageBox.Show("Error!");
+                return;
+            }
+
+            ProjectName = nameBox.Text.Trim();
+            ProjectDescription = descriptionBox.Text.Trim();
+
+            var selectedColor = colorPicker.SelectedColor ?? Colors.LightGray;
+            ProjectColor = selectedColor.ToString();
+
+            DialogResult = true;
+            Close();
+        }
+    }   
+}
