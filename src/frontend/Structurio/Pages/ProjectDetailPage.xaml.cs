b45a5cb2656173dee84da754bb716694aee453ba
FeldherrWeiprecht <drag.moser@outlook.com>
2025-06-12 08:58:41 +0200

    style: added ProjectFilesPage and connection between ProjectDetailPage and ProjectFilesPage

--- src/frontend/Structurio/Pages/ProjectDetailPage.xaml.cs
+++ src/frontend/Structurio/Pages/ProjectDetailPage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -32,7 +35,31 @@ namespace Structurio.Pages
             nameText.Text = project.Name.ToUpper();
             var brush = (SolidColorBrush)new BrushConverter().ConvertFromString(project.Color);
             (nameText.Parent as Border).Background = brush;
+
             contentFrame.Navigate(new KanbanPage(project));
+        }
+
+        private void UncheckAllMenuItems(object sender)
+        {
+            foreach (var child in LogicalTreeHelper.GetChildren(TopbarMenuPanel))
+            {
+                if (child is ToggleButton btn && !ReferenceEquals(btn, sender))
+                {
+                    btn.IsChecked = false;
+                }
+            }
+        }
+
+        private void kanban_Click(object sender, RoutedEventArgs e)
+        {
+//            UncheckAllMenuItems(sender);
+//            contentFrame.Navigate(new KanbanPage(project));
+        }
+
+        private void files_Click(object sender, RoutedEventArgs e)
+        {
+            UncheckAllMenuItems(sender);
+            contentFrame.Navigate(new ProjectFilesPage());
         }
 
         private void back_Click(object sender, RoutedEventArgs e)
